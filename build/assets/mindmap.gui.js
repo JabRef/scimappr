c1f2cd1cc96bf0070f9b060bb5464a2b9779345c
mdandyf <mochamad.dandy@gmail.com>
2017-06-24 08:48:33 +0200

    Latest Update
    1. Change mindmap.html section <body onload = dragdrop()>
    
    2 move GUI section into mindmap.gui.js
    
    3. move PDF section into mindmap.pdf.js

---
+++ build/assets/mindmap.gui.js
@@ -0,0 +1,124 @@
+// ============================================================================================= //
+// JQuery Section
+// ============================================================================================= //
+
+
+var draggableOptions = {
+				helper: 'clone',
+				containment:'frame',
+				opacity:'0.5',
+				revert: 'invalid',
+				appendTo: 'body',
+				stop: function (ev, ui) {	
+				var pos = $(ui.helper).offset();
+				objName = "#clonediv"
+				$(objName).css({
+					"left": pos.left,
+					"top": pos.top
+				});
+				$(objName).removeClass("drag");
+				}
+}
+	
+var droppableOptions = {
+		drop: function (ev, ui) {
+			var selected_node = _jm.get_selected_node(); // select node when mouseover
+			if(!selected_node){
+				prompt_info('please select a node first.');
+				return;
+			}
+			var nodeid = ui.helper.prevObject.context.id;
+			var topic = ui.helper.prevObject.context.innerHTML;
+			var node = _jm.add_node(selected_node, nodeid, topic);
+		}
+}
+
+var objName = "";
+
+var drag_drop = function() {
+			$( ".drag" ).draggable(draggableOptions);
+			$(".drop").droppable(droppableOptions);
+}
+
+var refreshUiPdf = function (data) {
+
+	$("#loading-data").remove();
+	$("#drag").remove();
+	var file_title = "<li style='cursor: no-drop; background-color: #ccc;padding: 10px 18px'>" + data.fileName + "</li>";
+	$(file_title).appendTo(".list-group");
+	for(var m = 0; m < data.data.length; m++) {
+
+		// Build JSON File
+		var json_listmaker = {
+				data: {
+					id: data.data[m].id, topic: data.data[m].topic
+				}
+			}
+
+		var json_result = JSON.stringify(json_listmaker);
+
+		// Parse JSON File back
+
+		var objekt = JSON.parse(json_result);
+		
+		setDynamicHtml(objekt);
+	}
+
+}
+
+var setDynamicHtml = function(objekt) {
+	var html_dynamic = "<li id=" + objekt.data.id +  ">" + objekt.data.topic + "</li>";
+			
+	$(html_dynamic).appendTo(".list-group").draggable(draggableOptions);
+			
+	document.getElementById(objekt.data.id).className += " drag list-group-item";
+	document.getElementById(objekt.data.id).style += "z-index: 5";
+
+	// Hide nodes that are already in the mindmap
+	if (_jm.mind.nodes[objekt.data.id]) {
+		$('#'+objekt.data.id).hide();
+	}
+	
+}	
+
+// Update the annotation panel on each MindMap event
+_jm.add_event_listener(function () {
+		var mindmap = _jm.get_data();
+		var nodes = $('.list-group-item').get();
+		nodes.forEach(function (node) {
+			if (_jm.mind.nodes[node.id]) {
+				$('#'+node.id).hide();
+			} else {
+				$('#'+node.id).show();
+			}
+		});
+
+		// Update localStorage with the current mindmap data on every update
+		window.localStorage.setItem('json_data', JSON.stringify(_jm.get_data()));
+});
+
+function myCloseConfirmation() {
+	if(confirm('are you sure to close?'))
+		return true;
+    else
+		return false;
+}
+
+function myExitConfirmation() {
+	if(confirm('are you sure to exit?'))
+		return true;
+	else
+		return false;
+}
+
+function getUserInfo() {
+		try {
+			var network = new ActiveXObject('WScript.Network');
+			prompt_info('User ID : ' + network.UserName + '\nComputer Name : ' + network.ComputerName + '\nDomain Name : ' + network.UserDomain);
+			document.getElementById('<%= currUserID.ClientID %>').value = network.UserName;
+			document.getElementById('<%= currMachineName.ClientID %>').value = network.ComputerName;
+			document.getElementById('<%= currMachineDOmain.ClientID %>').value = network.UserDomain;
+		} catch (e) {
+			
+		}
+}
---
+++ build/assets/mindmap.pdf.js
@@ -0,0 +1,113 @@
+"use strict";
+
+	// ============================================================================================= //
+	// Function PDF Section
+	// ============================================================================================= //
+	
+	//'use strict';
+	PDFJS.workerSrc = '/scimappr/build/assets/pdf.worker.js';
+    PDFJS.disableWorker = true;
+    PDFJS.disableRange = true;
+	
+	var dir = "/scimappr/doc/";
+	var listFile = [];
+	var files,
+      file,
+      extension,
+      input = document.getElementById("fileURL"),
+      mindmap = {};
+
+
+	// When PDF is refreshed
+	var mainRefreshPdf = function() {
+		populateDir().then(function(v){
+			listFile = v;
+		Promise.all(listFile).then(function(v) {
+				for(var k in listFile) {
+				loadPDF(listFile[k], listFile[k].replace("http://", "").replace("localhost/", ""))
+				}
+			});
+		});
+	}
+	
+	
+	// Keep the records of PDF File in a folder
+	var populateDir = function() {
+		return new Promise(function(resolve, reject) {
+			var xhr = $.ajax({
+            //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+			type:"GET",
+            url: dir,
+            success: function (data) {
+				
+               //List all pdfs file names in the page
+               $(data).find("a:contains(" + ".pdf" + ")").each(function (e) {
+					var filename = this.href.replace("http://", "").replace("localhost/", "").replace("scimappr/","doc/");
+					getLastMod("/scimappr/" + filename, function(v) {
+						console.log(v);
+					});
+					listFile.push(filename);
+					resolve (listFile);
+				});
+            }
+            });
+		});
+	}
+	
+	// get the annotations in a PDF File	
+	var loadPDF = function (filePath, fileName) {
+      PDFJS.getDocument(filePath).then(function (pdf) {
+        var promises = [], promise;
+        for (var i = 1; i < pdf.numPages; i++) {
+          promise = pdf.getPage(i).then(function (page) {
+            return page.getAnnotations();
+          });
+          promises.push(promise);
+        }
+        var ignoreList = ['Link'];
+        Promise.all(promises).then(function (pages) {
+          var items = [];
+          pages.forEach(function(annotations) {
+            annotations
+              .filter(function (annotation) {
+                return !ignoreList.includes(annotation.subtype)
+              })
+              .forEach(function(annotation) {
+			  if((annotation.contents != "") && (annotation.subtype == "Popup")) {
+			  items.push({
+                  id: getHashFunction(fileName + " " + annotation.contents),
+                  subtype: annotation.subtype,
+                  title: annotation.title,
+                  topic: annotation.contents
+                });
+			  }
+
+              });
+            });
+          refreshUiPdf({ fileName: fileName, data: items });
+        })
+      });
+    }
+
+	// generate random string
+	var getRandomString = function (num) {
+      num = num || 6;
+      return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, num);
+    };
+
+	// generate Hash function for Id
+	var getHashFunction = function (input) {
+		var hash = hex_md5("string");
+		var hmac = hex_hmac_md5("19", input);
+		return hmac;
+	}
+
+	// generate last modified date & time
+	var getLastMod = function(url, cb) {
+		var req = new XMLHttpRequest();
+		req.open("GET", url);
+		req.send(null);
+		req.addEventListener("load", function() {
+			cb(req.getResponseHeader("Last-Modified"));
+		}, false);
+	}
